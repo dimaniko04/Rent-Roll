13457b10a4e2dd5f25b0faa99fc1879f77ef9ba9
DimaNiko04 <dimanikora04@gmail.com>
2025-05-22 03:16:43 +0300

    Configure hot reload for docker container.

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <main className="min-h-full flex items-center justify-center bg-red-100">
+      <h1 className="text-5xl text-blue-400">Hello World!</h1>
+    </main>
+  );
+};
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tailwindcss(), react()],
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+  },
+});
