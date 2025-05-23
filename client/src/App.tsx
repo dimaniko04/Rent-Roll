7413d7aa7fd19bf16a4e585d2b12401b60f16e57
DimaNiko04 <dimanikora04@gmail.com>
2025-05-23 23:47:13 +0300

    Added tanstack router for page navigation. Configured absolute paths.

--- client/src/App.tsx
+++
@@ -1,7 +0,0 @@
-export const App = () => {
-  return (
-    <main className="min-h-full flex items-center justify-center bg-red-100">
-      <h1 className="text-5xl text-blue-400">Hello World!</h1>
-    </main>
-  );
-};
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    TanStackRouterVite({
+      target: "react",
+      autoCodeSplitting: true,
+      routesDirectory: "src/pages",
+    }),
+    tailwindcss(),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": "/src/components",
+      "@assets": "/src/assets",
+      "@lib": "/src/lib",
+    },
+  },
   server: {
     port: 3000,
     strictPort: true,
