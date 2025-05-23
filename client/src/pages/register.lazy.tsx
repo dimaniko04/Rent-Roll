af0dd7a5b62ac26e8e6aa785774ee669e8032834
DimaNiko04 <dimanikora04@gmail.com>
2025-05-24 01:54:11 +0300

    Imported custom fonts.

--- client/src/pages/register.lazy.tsx
+++ client/src/pages/register.lazy.tsx
@@ -1,3 +1,4 @@
+import companyLogo from "@assets/dice-logo.svg";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/register")({
@@ -5,5 +6,20 @@ export const Route = createLazyFileRoute("/register")({
 });
 
 function RouteComponent() {
-  return <div>Hello "/register"!</div>;
+  return (
+    <div>
+      <div>
+        <img
+          src={companyLogo}
+          className="mx-auto my-[-20px]"
+          alt="Logo"
+          width={196}
+          height={196}
+        />
+        <h2 className="font-dynapuff text-5xl text-center">Rent&Roll</h2>
+      </div>
+      <h1 className="text-center">Register</h1>
+      <h1 className="text-center font-roboto">Register (Roboto)</h1>
+    </div>
+  );
 }
